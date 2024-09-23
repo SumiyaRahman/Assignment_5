622c2ef57b2a6c68acf638624ba8261f477361ec
SumiyaRahman <sumiarahman01739@gmail.com>
2024-09-23 19:46:55 +0600

    added functionalities to donation card 1 (Donate for Flood at Noakhali, Bangladesh)

--- JS/donate.js
+++ JS/donate.js
@@ -1,19 +1,43 @@
-console.log("hello");
-
-let noaTotal = document.getElementById("noa-total");
-
 // redirect to FAQ page
 function toFAQpage() {
     window.location.href = "FAQ.html";
 }
 
 // donation button
-function toIndexHtml() {
-    window.location.href = "index.html";
-}
+document.getElementById("show-donation").addEventListener('click', function(){
+    document.getElementById("remove-donation-section").classList.remove('hidden');
+
+    document.getElementById("block-history").classList.add('hidden');
+});
+
 // history button
 document.getElementById("show-history").addEventListener('click', function(){
     document.getElementById("block-history").classList.remove('hidden');
 
     document.getElementById("remove-donation-section").classList.add('hidden');
 });
+
+// add noa money
+document.getElementById('donate-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    // get input value
+    let donationValue = document.getElementById("donation-value").value;
+    
+    // get current amount
+    let noaTotal = document.getElementById("noa-total").innerText;
+    
+    // get input value + get current value
+    let newNoaTotal = parseFloat(donationValue) + parseFloat(noaTotal);
+
+    // update the balance
+    document.getElementById("noa-total").innerText = newNoaTotal;
+
+    // total balance decrease
+    let totalBalance = document.getElementById("total-balance").innerText;
+    
+    let newTotalBalance = parseFloat(totalBalance) - parseFloat(donationValue);
+
+    // update total balance
+    document.getElementById("total-balance").innerText = newTotalBalance;
+})

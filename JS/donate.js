1174240453875c0086313c271495c402fbf8745e
SumiyaRahman <sumiarahman01739@gmail.com>
2024-09-23 20:03:55 +0600

    added more functionalities to donation and history button

--- JS/donate.js
+++ JS/donate.js
@@ -8,6 +8,18 @@ document.getElementById("show-donation").addEventListener('click', function(){
     document.getElementById("remove-donation-section").classList.remove('hidden');
 
     document.getElementById("block-history").classList.add('hidden');
+
+    document.getElementById("show-donation").classList.add('bg-primary');
+    document.getElementById("show-donation").classList.remove('border');
+    document.getElementById("show-donation").classList.remove('border-textSecondary');
+    document.getElementById("show-donation").classList.remove('text-textSecondary');
+    document.getElementById("show-donation").classList.add('text-textPrimary');
+
+    document.getElementById("show-history").classList.remove('bg-primary');
+    document.getElementById("show-history").classList.add('border');
+    document.getElementById("show-history").classList.add('border-textSecondary');
+    document.getElementById("show-history").classList.add('text-textSecondary');
+    document.getElementById("show-history").classList.remove('text-textPrimary');
 });
 
 // history button
@@ -15,6 +27,18 @@ document.getElementById("show-history").addEventListener('click', function(){
     document.getElementById("block-history").classList.remove('hidden');
 
     document.getElementById("remove-donation-section").classList.add('hidden');
+
+    document.getElementById("show-history").classList.add('bg-primary')
+    document.getElementById("show-history").classList.remove('border');
+    document.getElementById("show-history").classList.remove('border-textSecondary');
+    document.getElementById("show-history").classList.remove('text-textSecondary');
+    document.getElementById("show-history").classList.add('text-textPrimary');
+
+    document.getElementById("show-donation").classList.remove('bg-primary');
+    document.getElementById("show-donation").classList.add('border');
+    document.getElementById("show-donation").classList.add('border-textSecondary');
+    document.getElementById("show-donation").classList.add('text-textSecondary');
+    document.getElementById("show-donation").classList.remove('text-textPrimary');
 });
 
 // add noa money
